f8f64e045259b5c0e768a07f27b07e6c4ce892b7
Jonathon Toon <jonathontoon@gmail.com>
2020-01-09 20:51:25 +1100

    update default memo text

--- src/js/globals.js
+++ src/js/globals.js
@@ -3,7 +3,7 @@ export const GRID_SIZE = 10;
 
 export const DEFAULT_MEMO = {
   id: "9346dc6c-f982-48ce-9e21-f6a334b367b3",
-  text: "Manifest is a grid-based pinboard for note taking.\n\nSimply click anywhere to create as many notes as you want and snap them to the grid set by your current window size. All notes can be be moved, resized and deleted.\n\nYour browser's local storage is also utilized to save notes between sessions.\n\nManifest is free and completely open-source. Licensed under the GNU General Public License v2.0.\n\nIf you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
+  text: "Manifest is a grid-based pinboard for note taking.\n\nSimply click and drag anywhere to create a note and snap it to the grid set by your current window size. All notes can be be moved, resized and deleted.\n\nYour browser's local storage is also utilized to save notes between sessions.\n\nManifest is free and completely open-source. Licensed under the GNU General Public License v2.0.\n\nIf you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
   position: {
     top: 50,
     left: 50
