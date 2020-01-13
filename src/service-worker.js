2a95deddf31db6d51667e7c5acec055be855a14b
Jonathon Toon <jonathontoon@gmail.com>
2020-01-13 16:18:24 +1100

    remove window

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,27 +7,27 @@ const cacheFiles = [
   "./sass/index.css"
 ];
 
-window.addEventListener("install", function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     // Open the cache
-    window.caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function (cache) {
       // Add all the default files to the cache
       console.log("[ServiceWorker] Caching cacheFiles");
-      return window.cache.addAll(cacheFiles);
+      return cache.addAll(cacheFiles);
     })
   );
 });
 
-window.addEventListener("activate", function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     // Get all the cache keys (cacheName)
-    window.caches.keys().then(function (cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(cacheNames.map(function (thisCacheName) {
         // If a cached item is saved under a previous cacheName
         if (thisCacheName !== cacheName) {
           // Delete that cached file
           console.log("[ServiceWorker] Removing Cached Files from Cache - ", thisCacheName);
-          return window.caches.delete(thisCacheName);
+          return caches.delete(thisCacheName);
         }
       }));
     })
