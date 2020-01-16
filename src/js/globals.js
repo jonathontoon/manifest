a7ca2a0939d62faf2ced2d2df9ef180ba8beb80c
Jonathon Toon <jonathontoon@gmail.com>
2020-01-16 15:02:01 +1100

    Append missing export

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,8 +1,8 @@
 export const MARGIN = 48;
 export const GRID_SIZE = 10;
 
-const DRAG_INDEX = "99999";
-const STATIC_INDEX = "99998";
+export const DRAG_INDEX = "99999";
+export const STATIC_INDEX = "99998";
 
 export const DEFAULT_MEMO = {
   id: "9346dc6c-f982-48ce-9e21-f6a334b367b3",
