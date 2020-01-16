09d10766c96d9b1313c89172ce119d7e6655760f
Jonathon Toon <jonathontoon@gmail.com>
2020-01-17 10:47:50 +1100

    Update images

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 const cacheName = "manifest-1.0.0";
 
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll([
@@ -14,16 +14,14 @@ self.addEventListener('install', function (e) {
   );
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener("activate", function (event) {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.open(cacheName)
       .then(function (cache) { cache.match(event.request, {ignoreSearch: true}) })
-      .then(function (response) {
-      return response || fetch(event.request);
-    })
+      .then(function (response) { return response || fetch(event.request); })
   );
 });
