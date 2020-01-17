6286f93d0d570ad4983df42b1ecda24d689bbf83
Jonathon Toon <jonathontoon@gmail.com>
2020-01-17 13:28:36 +1100

    V1.1.0 (#86)
    * update scrollbar offset css
    
    * Add favicons
    
    * Focus on create or interact
    
    * Simplify loop
    
    * Update const values
    
    * Add to globals
    
    * Append missing export
    
    * Initial bounds restriction
    
    * Fix minify compile error
    
    * Simple bounds for resize
    
    * Update images
    
    * Add back source maps
    
    * Update ref
    
    * Remove scalable no
    
    * Update object
    
    * add hash
    
    * Remove window
    
    * Update manifest
    
    * Update service workers
    
    * Update images
    
    * Update windows icons
    
    * Firefox specific scrollbars
    
    * Update path
    
    * remove default, catch for touch array
    
    * Add maskable images
    
    * update icons
    
    * Update touches detection
    
    * add icons to manifest
    
    * cache favicons

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,20 +7,40 @@ self.addEventListener("install", function (e) {
         "./",
         "./index.html",
         "./js.00a46daa.js",
-        "./js.00a46daa.css"
+        "./js.00a46daa.css",
+        "favicon-16.a76ef598.png",
+        "favicon-32.f19d3578.png",
+        "favicon-48.d7bc7928.png",
+        "favicon-57.83a678dc.png",
+        "favicon-72.6280bbd5.png",
+        "favicon-76.50a865a6.png",
+        "favicon-96.2ff009b1.png",
+        "favicon-120.0b2d969e.png",
+        "favicon-128.b59a3681.png",
+        "favicon-144.3f07b896.png",
+        "favicon-152.91b30bc8.png",
+        "favicon-167.f9b1d528.png",
+        "favicon-180.2188ded0.png",
+        "favicon-192.ae26388c.png",
+        "favicon-228.98337c4a.png",
+        "favicon-384.9fd968c5.png",
+        "favicon-512.61aaf624.png",
+        "favicon.3697f982.svg",
+        "favicon.d42470cd.ico",
+        "browserconfig.d20e1c21.xml"
       ]).then(function () { self.skipWaiting(); });
     })
   );
 });
 
-self.addEventListener("activate", function (event) {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", function (e) {
+  encodeURIComponent.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
     caches.open(cacheName)
-      .then(function (cache) { cache.match(event.request, { ignoreSearch: true }); })
-      .then(function (response) { return response || fetch(event.request); })
+      .then(function (cache) { cache.match(e.request, { ignoreSearch: true }); })
+      .then(function (response) { return response || fetch(e.request); })
   );
 });
