67e8277e6b879ae95c503a6a56489e04d783d6bd
Jonathon Toon <jonathontoon@gmail.com>
2020-01-20 21:45:07 +1100

    V1.1.1 > Develop (#110)
    * disable select
    
    * disable select properly
    
    * fix toggle support on edge
    
    * remove console
    
    * disable rubber banding
    
    * add passive

--- src/js/globals.js
+++ src/js/globals.js
@@ -6,13 +6,13 @@ export const STATIC_INDEX = "99998";
 
 export const DEFAULT_MEMO = {
   id: "9346dc6c-f982-48ce-9e21-f6a334b367b3",
-  text: "Manifest is a grid-based pinboard for note taking.\n\nSimply click and drag anywhere to create a note and snap it to the grid set by your current window size. All notes can be moved, resized and deleted.\n\nYour browser's local storage is also utilized to save notes between sessions.\n\nManifest is free and completely open-source. Licensed under the GNU General Public License v2.0.\n\nIf you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
+  text: "Manifest is a grid-based pinboard for note taking.\n\nSimply click and drag anywhere to create a memo and snap it to the grid. All memos can be moved, resized, deleted and are saved in your browser's local storage between sessions.\n\nTo toggle between dark and light modes you can press Alt + T.\n\nManifest is free and completely open-source. If you find a bug or have a suggestion feel free to file an issue on Github.\n\nhttps://github.com/jonathontoon/manifest/issues",
   position: {
     top: 50,
     left: 50
   },
   size: {
     width: 379,
-    height: 339
+    height: 309
   }
 };
